93381916efa1d496b55bb6b775802d90890d2ee7
hyun-jii <tldnals49@naver.com>
2020-04-20 18:15:43 +0900

    # 날짜별 open / issue 수 차트 수정

--- board/src/main/java/com/example/board/Service/ListService.java
+++ board/src/main/java/com/example/board/Service/ListService.java
@@ -7,6 +7,9 @@ import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -90,38 +93,16 @@ public class ListService {
     // 차트
     public JSONArray getChart(String urlString, String token) throws Exception {
 
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        // 오늘
-        Calendar ca = Calendar.getInstance();
-        String today = dateFormat.format(ca.getTime());
-        // 1일전
-        ca.add(Calendar.DATE, -1);
-        String today_1 = dateFormat.format(ca.getTime());
-        // 2일전
-        ca.add(Calendar.DATE, -1);
-        String today_2 = dateFormat.format(ca.getTime());
-        // 3일전
-        ca.add(Calendar.DATE, -1);
-        String today_3 = dateFormat.format(ca.getTime());
-        // 4일전
-        ca.add(Calendar.DATE, -1);
-        String today_4 = dateFormat.format(ca.getTime());
-
-        // 해당 날짜 open, closed 이슈 count
-        int openCount = 0;
-        int closedCount = 0;
-        // open, closed 총 이슈 count
-        int closed = 0;
-        int open = 0;
-
-        JSONObject list1 = new JSONObject();
-        JSONObject list2 = new JSONObject();
-        JSONObject list3 = new JSONObject();
-        JSONObject list4 = new JSONObject();
-        JSONObject list5 = new JSONObject();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 
         JSONArray array = new JSONArray();
+        Map<String, Integer> openMap = new LinkedHashMap<>();
+        Map<String, Integer> closedMap = new LinkedHashMap<>();
         int i = 1;
+        int openCount = 1;
+        int closedCount = 1;
+        int openIssue = 0;
+        int closedIssue = 0;
 
         while (true) {
             URL url = new URL(urlString + "&page=" + i);
@@ -136,69 +117,38 @@ public class ListService {
                 JSONObject ob = (JSONObject) temp.get(j);
                 String state = (String) ob.get("closed_at");
 
-                // closed_at == null , open 이슈
                 if (state == null) {
-                    open++;
+                    openIssue++;
                     state = (String) ob.get("created_at");
+                    String date = format.format(format.parse(state));
 
-                    Date date = dateFormat.parse(state);
-                    String issueDate = dateFormat.format(date);
-
-                    if (issueDate.equals(today)) {
-                        openCount++;
-                        list1.put("created_at", openCount);
-                    } else if (issueDate.equals(today_1)) {
-                        openCount++;
-                        list2.put("created_at", openCount);
-                    } else if (issueDate.equals(today_2)) {
-                        openCount++;
-                        list3.put("created_at", openCount);
-                    } else if (issueDate.equals(today_3)) {
-                        openCount++;
-                        list4.put("created_at", openCount);
-                    } else if (issueDate.equals(today_4)) {
-                        openCount++;
-                        list5.put("created_at", openCount);
+                    if (openMap.containsKey(date)) {
+                        openMap.put(date, ++openCount);
+                        continue;
                     }
-                }
-                // closed 이슈
-                else {
-                    closed++;
 
-                    Date date = dateFormat.parse(state);
-                    String issueDate = dateFormat.format(date);
+                    openMap.put(date, openCount);
 
-                    if (issueDate.equals(today)) {
-                        closedCount++;
-                        list1.put("closed_at", closedCount);
-                    } else if (issueDate.equals(today_1)) {
-                        closedCount++;
-                        list2.put("closed_at", closedCount);
-                    } else if (issueDate.equals(today_2)) {
-                        closedCount++;
-                        list3.put("closed_at", closedCount);
-                    } else if (issueDate.equals(today_3)) {
-                        closedCount++;
-                        list4.put("closed_at", closedCount);
-                    } else if (issueDate.equals(today_4)) {
-                        closedCount++;
-                        list5.put("closed_at", closedCount);
+                } else {
+                    closedIssue++;
+                    String date = format.format(format.parse(state));
+
+                    if (closedMap.containsKey(date)) {
+                        closedMap.put(date, ++closedCount);
+                        continue;
                     }
+                    closedMap.put(date, closedCount);
                 }
 
             }
 
             i++;
         }
-        list1.put("openCount", open);
-        list1.put("closedCount", closed);
-        System.out.println("총 이슈 :" + openCount);
-        System.out.println("닫힌 이슈 :" + closedCount);
-        array.add(list1);
-        array.add(list2);
-        array.add(list3);
-        array.add(list4);
-        array.add(list5);
+        openMap.put("openIssue", openIssue);
+        closedMap.put("closedIssue", closedIssue);
+        array.add(openMap);
+        array.add(closedMap);
+
         return array;
     }
 
